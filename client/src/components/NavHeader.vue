0f1ec8dd7657fa337bf5a824e9ee576a123f1664
leiger <leiger@bupt.edu.cn>
2018-09-16 21:32:44 -0400

    add login

--- client/src/components/NavHeader.vue
+++ client/src/components/NavHeader.vue
@@ -9,12 +9,36 @@
       <b-collapse is-nav id="nav_collapse">
 
         <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
+
           <b-nav-item href="#" disabled>Disabled</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right user info -->
         <b-navbar-nav class="ml-auto">
+
+          <template v-if="nickName === ''">
+            <b-nav-item href="#" @click="loginModel">Login</b-nav-item>
+          </template>
+          <template v-else>
+            <b-nav-item href="#">{{nickName}}</b-nav-item>
+          </template>
+
+          <!--login modal-->
+          <b-modal ref="loginRef" title="LOGIN" centered ok-title="SUBMIT" @ok="handleOk">
+            <div class="d-block">
+              <b-alert :variant="loginInfo.color" dismissible :show="loginInfo.show">{{ loginInfo.msg }}</b-alert>
+              <b-form @submit.stop.prevent="onSubmit">
+                <b-form-group label="Username:" label-for="username">
+                  <b-form-input v-model="login.username" id="username" type="text" required
+                                placeholder="Enter your username"></b-form-input>
+                </b-form-group>
+                <b-form-group label="Password:" label-for="password">
+                  <b-form-input v-model="login.password" id="password" type="password" required
+                                placeholder="Enter password"></b-form-input>
+                </b-form-group>
+              </b-form>
+            </div>
+          </b-modal>
 
           <b-nav-item-dropdown right>
             <!-- Using button-content slot -->
@@ -31,13 +55,67 @@
 
 <script>
   //  import HeaderComponent from './components/header.vue'
+  import axios from 'axios';
 
   export default {
     data() {
-      return {}
+      return {
+        login: {
+          username: '',
+          password: ''
+        },
+        loginInfo: {
+          show: false,
+          msg: '',
+          color: ''
+        },
+        nickName: '',
+      }
     },
-    components: {
-//      HeaderComponent
+    methods: {
+      loginModel() {
+        this.$refs.loginRef.show();
+      },
+      handleOk(evt) {
+//        prevent default close
+        evt.preventDefault();
+        console.log('123');
+        if (!this.login.username || !this.login.password) {
+          this.loginInfo.show = true;
+          this.loginInfo.color = 'danger';
+          this.loginInfo.msg = "Please input your username and password";
+        }
+        else {
+          this.loginInfo.show = false;
+          this.onSubmit()
+        }
+      },
+      onSubmit() {
+        axios.post('/users/login', {
+          username: this.login.username,
+          password: this.login.password
+        }).then(res => {
+          let data = res.data;
+          if (data.status === '0') {
+            this.loginInfo.show = true;
+            this.loginInfo.color = 'success';
+            this.loginInfo.msg = 'Login Success';
+            // to-do
+            this.nickName = data.result.username;
+
+            setTimeout(() => {
+              this.$refs.loginRef.hide();
+            }, 1000);
+          }
+          else if (data.status === '2') {
+            this.loginInfo.show = true;
+            this.loginInfo.color = 'warning';
+            this.loginInfo.msg = "wrong username or password!";
+          }
+        });
+//
+//        alert("submit success");
+      }
     }
   }
 </script>
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
+let Users = require('./../models/users');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/login', function(req, res, next) {
+  let param = {
+    userName: req.body.username,
+    userPwd: req.body.password
+  };
+  Users.findOne(param, function(err, doc) {
+    console.log(doc);
+    if(err) {
+      res.json({
+        status: '1',
+        msg: err.message
+      });
+    }
+    else {
+      if(doc) {
+        // cookie and session
+        res.cookie("userId", doc.userId, {
+          path: '/',
+          maxAge: 1000*60*60
+        });
+        // req.session.user = doc;
+
+        res.json({
+          status: '0',
+          msg: '',
+          result: {
+            username: doc.userName
+          }
+        })
+      }
+      else {
+        // can't find the user
+        res.json({
+          status: '2',
+          msg: ''
+        })
+      }
+    }
+  });
 });
 
 module.exports = router;
